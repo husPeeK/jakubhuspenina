17f10a2ea44b8bf09ee3812dc6f6832a3c793746
husPeeK <119861261+husPeeK@users.noreply.github.com>
2024-10-06 13:13:52 +0200

    Projekt Cmake Huspenina
    hotovo

---
+++ CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,23 @@
+﻿// CMakeProject1.cpp: Definuje vstupní bod pro aplikaci.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	float cas, vzd;
+	const float rs = 340.0;
+
+	cout << "Zadejte cas v sekundach, za ktery se ozval hrom po blesku: ";
+	if (!(cin >> cas)) {
+		cout << "Prosim, zadej cislo." << endl;
+		return 1;
+	}
+
+	vzd = cas * rs;
+
+	cout << "Blesk uhodil cca" << vzd << " metru od vas." << endl;
+	return 0;
+}
