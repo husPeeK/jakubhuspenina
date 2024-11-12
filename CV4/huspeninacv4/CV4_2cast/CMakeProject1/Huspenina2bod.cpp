1cf8cd042a697c69d902f1a556c9883c4da92af9
husPeeK <119861261+husPeeK@users.noreply.github.com>
2024-11-12 12:12:00 +0100

    Add files via upload

---
+++ CV4/huspeninacv4/CV4_2cast/CMakeProject1/Huspenina2bod.cpp
@@ -0,0 +1,45 @@
+﻿// CMakeProject1.cpp: Definuje vstupní bod pro aplikaci.
+//
+
+#include "CMakeProject1.h"
+using namespace std;
+
+int isPrime(int c1) {
+	if (c1 < 0 || c1 > 100) {
+		return -1;
+	}
+
+	if (c1 == 0 || c1 == 1) {
+		return 0;
+	}
+
+	for (int i = 2; i * i <= c1; i++) {
+		if (c1 % i == 0) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+
+int main()
+{
+	int tc[] = { 1, 87, 9, 10, 23, 100, 102, 99, 45, -7 };
+	int pc = sizeof(tc) / sizeof(tc[0]);
+
+	for (int i = 0; i < pc; i++) {
+		int c = tc[i];
+		int v = isPrime(c);
+
+		if (v == -1) {
+			printf("Cislo %d je mimo interval 0-100\n", c);
+		}
+		else if (v == 1) {
+			printf("Cislo %d je prvocislo\n", c);
+		}
+		else {
+			printf("Cislo %d neni prvocislo\n", c);
+		}
+	}
+	return 0;
+}
